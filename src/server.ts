4ec9953bdbbf4c156dfdca117914e4a1abdd2604
André Knob <knobandre@gmail.com>
2025-02-18 00:41:15 -0300

    add schema validation for subscriptions endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,37 @@
 import { fastify } from 'fastify';
 import { fastifyCors } from '@fastify/cors';
-import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod';
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform
+} from 'fastify-type-provider-zod';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
+import { subscribeToEventRoute } from './routes/subscribe-to-event.route';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifyCors, {
-  origin: true
+app.register(fastifyCors);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1'
+    }
+  },
+  transform: jsonSchemaTransform
 });
 
-app.get('/hello', () => {
-  return 'hello';
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
 });
+
+app.register(subscribeToEventRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
