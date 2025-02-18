67d1e68dc842f174dca924fb0a9003544323cfad
André Knob <knobandre@gmail.com>
2025-02-18 01:15:38 -0300

    setup biome

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,39 @@
-import { fastify } from 'fastify';
-import { fastifyCors } from '@fastify/cors';
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
 import {
-  validatorCompiler,
+  type ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
-  ZodTypeProvider,
-  jsonSchemaTransform
-} from 'fastify-type-provider-zod';
-import { fastifySwagger } from '@fastify/swagger';
-import { fastifySwaggerUi } from '@fastify/swagger-ui';
-import { subscribeToEventRoute } from './routes/subscribe-to-event.route';
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event.route'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.register(fastifyCors);
+app.register(fastifyCors)
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
       title: 'NLW Connect',
-      version: '0.0.1'
-    }
+      version: '0.0.1',
+    },
   },
-  transform: jsonSchemaTransform
-});
+  transform: jsonSchemaTransform,
+})
 
 app.register(fastifySwaggerUi, {
-  routePrefix: '/docs'
-});
+  routePrefix: '/docs',
+})
 
-app.register(subscribeToEventRoute);
+app.register(subscribeToEventRoute)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
-});
+})
