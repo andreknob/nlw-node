a3068f1eb0ba582ed540d3c1d4cd84fb85274fe4
André Knob <knobandre@gmail.com>
2025-02-19 22:04:37 -0300

    feat: add get subscriber invite clicks route

---
+++ src/routes/get-subscriber-invite-clicks.route.ts
@@ -0,0 +1,33 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from '../env'
+import { accessInviteLink } from '../functions/access-invite-link'
+import { getSubscriberInviteCliks } from '../functions/get-subscriber-invite-clicks'
+
+export const getSubscriberInviteClicksRoute: FastifyPluginAsyncZod =
+  async app => {
+    app.get(
+      '/subscribers/:subscriberId/ranking/clicks',
+      {
+        schema: {
+          summary: 'Get subscriber invite clicks count',
+          tags: ['subscription'],
+          params: z.object({
+            subscriberId: z.string(),
+          }),
+          response: {
+            200: z.object({
+              count: z.number(),
+            }),
+          },
+        },
+      },
+      async request => {
+        const { subscriberId } = request.params
+
+        const { count } = await getSubscriberInviteCliks({ subscriberId })
+
+        return { count }
+      }
+    )
+  }
